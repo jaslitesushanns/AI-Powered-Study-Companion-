ab48deae79171b0a61cf616172250a54e7128dd1
jaslitesushanns <jaslitesushanns2026@gmail.com>
2026-07-23 18:37:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
+import streamlit as st
+
+from auth import (
+    create_session_table,
+    create_user_table
+)
+
+
+# Website settings
+st.set_page_config(
+    page_title="AI Study Companion",
+    page_icon="📚",
+    layout="wide"
+)
+
+
+# Create database tables
+create_user_table()
+create_session_table()
+
+
+# Home page
+
+st.title("📚 AI Study Companion")
+
+st.write(
+    "Your personal AI-powered study platform"
+)
+
+st.success("Website setup successful!")
+
 
